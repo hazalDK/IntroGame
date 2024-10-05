ac6f97e3e3991f3e8c6e47ee1e852ef2007ca594
Hazal Deniz Kara <94861070+hazalDK@users.noreply.github.com>
2024-10-05 20:58:54 +0100

    added the canvas test stuff too
    this is just to try the lab by myself

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem ;
+using UnityEngine . UI ;
+using TMPro;
+
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
+    private int count;
+    public int numPickups = 7;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    void Start (){
+        count = 0;
+        winText.text = "";
+        SetCountText();
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
@@ -16,5 +29,20 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(speed * Time.fixedDeltaTime * movement);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() {
+        scoreText.text = "Score: " + count.ToString();
+        if(count >= numPickups) {
+            winText.text = "You win!!";
+        }
     }
 }
